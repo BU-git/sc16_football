1e012e2d70644f3a00e0f41ef4c95de45f198d46
Taras Pavlyuk <Taras Pavlyuk>
2016-07-19 20:10:43 +0300

    SCFOOT-21 Add interactive maps to the Contacts page

--- imports/startup/client/index.js
+++ imports/startup/client/index.js
@@ -1,1 +1,3 @@
 import './routes.js';
+
+GoogleMaps.load();
--- imports/ui/pages/contact-page.js
+++ imports/ui/pages/contact-page.js
@@ -1,1 +1,42 @@
 import './contact-page.html';
+
+Template.contactPage.helpers({
+  rightMapOptions: function() {
+    // Make sure the maps API has loaded
+    if (GoogleMaps.loaded()) {
+      // Map initialization options
+      return {
+        center: new google.maps.LatLng(50.417435, 30.457508),
+        zoom: 17
+      };
+    }
+  },
+  leftMapOptions: function() {
+    // Make sure the maps API has loaded
+    if (GoogleMaps.loaded()) {
+      // Map initialization options
+      return {
+        center: new google.maps.LatLng(50.439343, 30.602430),
+        zoom: 17
+      };
+    }
+  }
+});
+
+Template.contactPage.onCreated(function() {
+  // We can use the `ready` callback to interact with the map API once the map is ready.
+  GoogleMaps.ready('rightMap', function(map) {
+    // Add a marker to the map once it's ready
+    var marker = new google.maps.Marker({
+      position: map.options.center,
+      map: map.instance
+    });
+  });
+  GoogleMaps.ready('leftMap', function(map) {
+    // Add a marker to the map once it's ready
+    var marker = new google.maps.Marker({
+      position: map.options.center,
+      map: map.instance
+    });
+  });
+});
