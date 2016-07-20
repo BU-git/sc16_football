55098111becf1bc09536a9b7a83b659bc622a10e
sebastiyan <tan9seb@gmail.com>
2016-07-20 23:28:14 +0300

    SCFOOT-14 Make Main page layout / added swiper

--- imports/ui/pages/home-page.js
+++ imports/ui/pages/home-page.js
@@ -1,1 +1,20 @@
 import './home-page.html';
+
+import { Template } from 'meteor/templating';
+
+Template.homePage.onRendered(function() {
+    var mySwiper = $('.swiper-container').swiper({
+    //Your options here:
+        mode:'horizontal',
+        loop: true,
+        autoplay: 3000,
+        speed: 500,
+        pagination: '.swiper-pagination',
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        paginationClickable: true,
+        effect: 'fade'
+        //etc..
+    });
+
+});
