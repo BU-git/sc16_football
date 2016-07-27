382d40392a518fe3fdf4413673b35e23b530fb0e
kobyzhcha <igorkobyzhcha@gmail.com>
2016-07-27 21:49:25 +0300

    SCFOOT-59 Create main page layout according to design

--- imports/ui/pages/home-page.js
+++ imports/ui/pages/home-page.js
@@ -3,7 +3,19 @@ import './home-page.html';
 import { Template } from 'meteor/templating';
 
 Template.homePage.onRendered(function() {
-    var mySwiper = $('.swiper-container').swiper({
+    var mySwiper = $('.slider--header').swiper({
+    //Your options here:
+        mode:'horizontal',
+        loop: true,
+        autoplay: 3000,
+        speed: 500,
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        paginationClickable: true,
+        effect: 'fade'
+        //etc..
+    });
+    var mySwiper = $('.slider--slogan').swiper({
     //Your options here:
         mode:'horizontal',
         loop: true,
