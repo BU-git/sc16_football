b4bc30c175620b92f4fcef1c070f6e5d00fbe322
Tanya Sebastiyan <Tanya Sebastiyan>
2016-07-28 17:15:12 +0300

    REFACT design main page

--- imports/ui/pages/home-page.js
+++ imports/ui/pages/home-page.js
@@ -37,16 +37,6 @@ Template.homePage.onCreated(function(){
 
 Template.homePage.helpers({
     news: function(){
-        return News.find({}, {sort: {createdAt: -1}, limit: 6}).fetch();
-    }
-});
-
-Template.homePage.onCreated(function(){
-    this.subscribe('news');
-});
-
-Template.homePage.helpers({
-    news: function(){
-        return News.find().fetch();
+        return News.find({}, {sort: {createdAt: 1}, limit: 6}).fetch();
     }
 });
