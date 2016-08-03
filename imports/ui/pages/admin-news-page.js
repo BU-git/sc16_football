6b1d1a6a7c687b5bffe1074b00844948beb1657d
Tanya Sebastiyan <Tanya Sebastiyan>
2016-08-04 00:34:06 +0300

    REFACT

--- imports/ui/pages/admin-news-page.js
+++ imports/ui/pages/admin-news-page.js
@@ -27,41 +27,11 @@ Template.adminNewsPage.helpers({
 
 
 Template.adminNewsPage.events({
-    'change #js-thumbnail-input': function (e, template) {
-        if (e.currentTarget.files && e.currentTarget.files[0]) {
-            // We upload only one file, in case 
-            // multiple files were selected
-            var upload = Images.insert({
-                file: e.currentTarget.files[0],
-                streams: 'dynamic',
-                chunkSize: 'dynamic'
-            }, false);
-
-            upload.on('start', function () {
-                template.currentUpload.set(this);
-            });
-
-            upload.on('end', function (error, fileObj) {
-                if (error) {
-                    alert('Error during upload: ' + error);
-                } else {
-                    template.thumbnailUrl = Images.link(fileObj);
-                }
-                template.currentUpload.set(false);
-            });
-
-            upload.start();
-        }
-    },
     'click .js-btn-remove-news': function(e, template){
         News.remove(this._id);
         sAlert.success('Новость удалена')
     },
-    // 'submit #js-form-edit-news': function(e, template) {
-    //     $('.js-modal-edit-news').modal('hide')
-    // },
     'click #js-open-modal': function(e, template) {
         Event.emit('openModal', {'addNews': true});
     }
-
 });
