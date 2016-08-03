5ec41a6ea9f0113b4e13560eb0b822dddf4b5892
Tanya Sebastiyan <Tanya Sebastiyan>
2016-08-04 01:18:05 +0300

    SCFOOT-146 btn show more for news page

--- imports/startup/client/index.js
+++ imports/startup/client/index.js
@@ -14,4 +14,18 @@ sAlert.config({
     onClose: _.noop
 });
 
+Meteor.Spinner.options = {
+  lines: 12, // The number of lines to draw
+  length: 10, // The length of each line
+  width: 4, // The line thickness
+  radius: 15, // The radius of the inner circle
+  corners: 0.7, // Corner roundness (0..1)
+  rotate: 0, // The rotation offset
+  direction: 1, // 1: clockwise, -1: counterclockwise
+  color: '#666', // #rgb or #rrggbb
+  speed: 1.3, // Rounds per second
+  trail: 60, // Afterglow percentage
+  zIndex: 100, // The z-index (defaults to 2000000000)
+};
+
 Event = new EventEmitter();
--- imports/ui/pages/news-page.js
+++ imports/ui/pages/news-page.js
@@ -5,11 +5,42 @@ import { Template } from 'meteor/templating';
 import { News } from '../../api/news.js';
 
 Template.newsPage.onCreated(function(){
-    this.subscribe('news');
+    var instance = this;
+    // initialize the reactive variables
+    instance.loaded = new ReactiveVar(0);
+    instance.limit = new ReactiveVar(6);
+    instance.autorun(function () {
+        // get the limit
+        var limit = instance.limit.get();
+        // subscribe to the posts publication
+        var subscription = instance.subscribe('news', limit);
+        // if subscription is ready, set limit to newLimit
+        if (subscription.ready()) {
+          instance.loaded.set(limit);
+        }
+    });
+    // cursor
+    instance.news = function() { 
+        return News.find({}, {limit: instance.loaded.get()});
+    }
 });
 
 Template.newsPage.helpers({
     news: function(){
-        return News.find().fetch();
+        return Template.instance().news();
+    },
+    hasMoreNews: function () {
+        return Template.instance().news().count() >= Template.instance().limit.get();
+    }
+});
+
+Template.newsPage.events({
+    'click #js-load-more-news': function (event, instance) {
+        event.preventDefault();
+        // get current value for limit, i.e. how many posts are currently displayed
+        var limit = instance.limit.get();
+        // increase limit by 6 and update it
+        limit += 6;
+        instance.limit.set(limit);
     }
 });
