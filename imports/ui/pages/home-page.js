71eebc8f0c5bacf9eedeb079920c81eb1ee1b3e4
Tanya Sebastiyan <Tanya Sebastiyan>
2016-08-04 15:32:10 +0300

    SCFOOT-170 The slider is very quickly move the text

--- imports/ui/pages/home-page.js
+++ imports/ui/pages/home-page.js
@@ -8,28 +8,24 @@ import { Session } from 'meteor/session';
 
 Template.homePage.onRendered(function() {
     var swiperHeader = $('.slider--header').swiper({
-    //Your options here:
         mode:'horizontal',
         loop: true,
-        autoplay: 3000,
+        autoplay: 10000,
         speed: 500,
         nextButton: '.slider--header .swiper-button-next',
         prevButton: '.slider--header .swiper-button-prev',
         paginationClickable: true,
         effect: 'fade'
-        //etc..
     });
     var swiperSlogan = $('.slider--slogan').swiper({
-    //Your options here:
         mode:'horizontal',
         loop: true,
-        autoplay: 3000,
+        autoplay: 10000,
         speed: 500,
         nextButton: '.slider--slogan .swiper-button-next',
         prevButton: '.slider--slogan .swiper-button-prev',
         paginationClickable: true,
         effect: 'fade'
-        //etc..
     });
     $('#js-week-calendar').fullCalendar({
         lang: 'ru',
