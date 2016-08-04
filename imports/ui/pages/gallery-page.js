468955ade4f9acdfbcfe0f19ea26afa8509cca77
kobyzhcha <igorkobyzhcha@gmail.com>
2016-08-05 01:26:27 +0300

    SCFOOT-160 Create static layout of Gallery with filters

--- imports/ui/pages/gallery-page.js
+++ imports/ui/pages/gallery-page.js
@@ -1,5 +1,25 @@
 import './gallery-page.html';
 
 Template.galleryPage.onRendered(function() {
-	$('.card').fancybox();
+    var $grid = $('.grid').isotope({
+        itemSelector: '.element-item',
+        layoutMode: 'fitRows'
+    });
+    var filterFns = {};
+
+    $('.filters-link-group').on( 'click', 'a', function() {
+        var filterValue = $( this ).attr('data-filter');
+
+        filterValue = filterFns[ filterValue ] || filterValue;
+        $grid.isotope({ filter: filterValue });
+    });
+
+    $('.link-group').each( function( i, linkGroup ) {
+        var $linkGroup = $( linkGroup );
+        $linkGroup.on( 'click', 'a', function() {
+            $linkGroup.find('.is-checked').removeClass('is-checked');
+            $( this ).addClass('is-checked');
+        });
+    });
 });
+
