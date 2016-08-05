b87220d16e3d2d9650fde655a7051db00e61f52b
Tanya Sebastiyan <Tanya Sebastiyan>
2016-08-05 17:36:42 +0300

    REFACT fix gallery page

--- imports/ui/pages/gallery-page.js
+++ imports/ui/pages/gallery-page.js
@@ -1,25 +1,21 @@
 import './gallery-page.html';
+import { Template } from 'meteor/templating';
+
+var $grid;
 
 Template.galleryPage.onRendered(function() {
-    var $grid = $('.grid').isotope({
-        itemSelector: '.element-item',
+    $grid = $('.gallery').isotope({
+        itemSelector: '.gallery__item',
         layoutMode: 'fitRows'
-    });
-    var filterFns = {};
-
-    $('.filters-link-group').on( 'click', 'a', function() {
-        var filterValue = $( this ).attr('data-filter');
-
-        filterValue = filterFns[ filterValue ] || filterValue;
-        $grid.isotope({ filter: filterValue });
-    });
-
-    $('.link-group').each( function( i, linkGroup ) {
-        var $linkGroup = $( linkGroup );
-        $linkGroup.on( 'click', 'a', function() {
-            $linkGroup.find('.is-checked').removeClass('is-checked');
-            $( this ).addClass('is-checked');
-        });
     });
 });
 
+Template.galleryPage.events({
+    'click .filter a': function(e){
+        e.preventDefault();
+        var filterValue = $(e.target).attr('data-filter');
+        $grid.isotope({ filter: filterValue });
+        $('.filter').find('.is-checked').removeClass('is-checked');
+        $(e.target).addClass('is-checked')
+    }
+})
