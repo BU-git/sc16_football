0236f7a3c9ef84aac96e23b1510aa21b4b940e08
Tanya Sebastiyan <Tanya Sebastiyan>
2016-08-07 02:28:18 +0300

    SCFOOT-174 In Admin profile there's no link 'Перейти на сайт' / Redesign header

--- imports/ui/components/header-admin.js
+++ imports/ui/components/header-admin.js
@@ -2,3 +2,21 @@ import './header-admin.html';
 
 import { Template } from 'meteor/templating';
 
+import { Enrolls } from '../../api/enrolls.js';
+
+Template.headerAdmin.onCreated(function(){
+    this.subscribe('enrolls');
+});
+
+Template.headerAdmin.helpers({
+    unreadCount: function(){
+        return Enrolls.find({"status": false}).count()
+    }
+});
+
+Template.headerAdmin.events({
+    'click #logout': function(e) {
+        e.preventDefault();
+        Meteor.logout();
+    }
+});
--- imports/ui/components/sidebar-admin.js
+++ imports/ui/components/sidebar-admin.js
@@ -1,21 +1,3 @@
 import './sidebar-admin.html';
 
 import { Template } from 'meteor/templating';
-import { Enrolls } from '../../api/enrolls.js';
-
-Template.sidebarAdmin.onCreated(function(){
-    this.subscribe('enrolls');
-});
-
-Template.sidebarAdmin.helpers({
-    unreadCount: function(){
-        return Enrolls.find({"status": false}).count()
-    }
-});
-
-Template.sidebarAdmin.events({
-    'click #logout': function(e) {
-        e.preventDefault();
-        Meteor.logout();
-    }
-});
