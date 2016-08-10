0450443e9eb17bf20227f71df54bb69719451b83
Tanya Sebastiyan <Tanya Sebastiyan>
2016-08-10 03:36:55 +0300

    REFACT fix mobile, refactoring code

--- imports/ui/pages/home-page.js
+++ imports/ui/pages/home-page.js
@@ -27,53 +27,103 @@ Template.homePage.onRendered(function() {
         paginationClickable: true,
         effect: 'fade'
     });
-    $('#js-week-calendar').fullCalendar({
-        lang: 'ru',
-        monthNames: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль',
-            'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
-        dayNames: ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'],
-        dayNamesShort: ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'],
-        monthNamesShort: ['янв', 'февр', 'март', 'апр', 'май', 'июн', 'июл', 'авг', 'сент', 'окт', 'нояб', 'дек'],
-        height: 'auto',
-        contentHeight: 'auto',
-        header: {
-            left: 'prev',
-            center: 'title',
-            right: 'next'
-        },
-        defaultView: 'basicWeek',
-        columnFormat: {
-            month: 'ddd',
-            week: 'dddd DD/MM',
-            day: 'dddd DD/MM'
-        },
-        windowResize: function(view) {
-            if ($(window).width() < 768) {
-               $('#js-week-calendar').fullCalendar('changeView', 'basicDay');
+    if ($(window).width() > 768) {
+        $('#js-week-calendar').fullCalendar({
+            lang: 'ru',
+            monthNames: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль',
+                'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
+            dayNames: ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'],
+            dayNamesShort: ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'],
+            monthNamesShort: ['янв', 'февр', 'март', 'апр', 'май', 'июн', 'июл', 'авг', 'сент', 'окт', 'нояб', 'дек'],
+            height: 'auto',
+            contentHeight: 'auto',
+            header: {
+                left: 'prev',
+                center: 'title',
+                right: 'next'
+            },
+            defaultView: 'basicWeek',
+            columnFormat: {
+                month: 'ddd',
+                week: 'dddd DD/MM',
+                day: 'dddd DD/MM'
+            },
+            windowResize: function(view) {
+                if ($(window).width() < 768) {
+                   $('#js-week-calendar').fullCalendar('changeView', 'basicDay');
+                }
+            },
+            events(start, end, timezone, callback) {
+                let data = Events.find().fetch()
+                if (data) {
+                    callback(data);
+                }
+            },
+            eventRender(event, element) {
+                element.find('.fc-content').html(
+                    '<div class="event__header"><div class="event__time"><span class="glyphicon glyphicon-time"></span> ' +
+                    event.time + '</div><div class="event__place"><span class="glyphicon glyphicon-map-marker"></span> ' +
+                    event.place + '</div></div><h4 class="event__title">' + event.title + '</h4>'
+                );
+            },
+            dayRender: function(date, cell) {
+                if (cell.hasClass('fc-today')) {
+                    var index = cell.index(); // get the td offset
+                    // find the corresponding item in header table
+                    var header = $('#js-week-calendar thead.fc-head th').eq(index);
+                    header.addClass('fc-today'); // update it with a class
+                }
             }
-        },
-        events(start, end, timezone, callback) {
-            let data = Events.find().fetch()
-            if (data) {
-                callback(data);
+        });
+    } else{
+        $('#js-week-calendar').fullCalendar({
+            lang: 'ru',
+            monthNames: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль',
+                'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
+            dayNames: ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'],
+            dayNamesShort: ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'],
+            monthNamesShort: ['янв', 'февр', 'март', 'апр', 'май', 'июн', 'июл', 'авг', 'сент', 'окт', 'нояб', 'дек'],
+            height: 'auto',
+            contentHeight: 'auto',
+            header: {
+                left: 'prev',
+                center: 'title',
+                right: 'next'
+            },
+            defaultView: 'basicDay',
+            columnFormat: {
+                month: 'ddd',
+                week: 'dddd DD/MM',
+                day: 'dddd DD/MM'
+            },
+            windowResize: function(view) {
+                if ($(window).width() < 768) {
+                   $('#js-week-calendar').fullCalendar('changeView', 'basicDay');
+                }
+            },
+            events(start, end, timezone, callback) {
+                let data = Events.find().fetch()
+                if (data) {
+                    callback(data);
+                }
+            },
+            eventRender(event, element) {
+                element.find('.fc-content').html(
+                    '<div class="event__header"><div class="event__time"><span class="glyphicon glyphicon-time"></span> ' +
+                    event.time + '</div><div class="event__place"><span class="glyphicon glyphicon-map-marker"></span> ' +
+                    event.place + '</div></div><h4 class="event__title">' + event.title + '</h4>'
+                );
+            },
+            dayRender: function(date, cell) {
+                if (cell.hasClass('fc-today')) {
+                    var index = cell.index(); // get the td offset
+                    // find the corresponding item in header table
+                    var header = $('#js-week-calendar thead.fc-head th').eq(index);
+                    header.addClass('fc-today'); // update it with a class
+                }
             }
-        },
-        eventRender(event, element) {
-            element.find('.fc-content').html(
-                '<div class="event__header"><div class="event__time"><span class="glyphicon glyphicon-time"></span> ' +
-                event.time + '</div><div class="event__place"><span class="glyphicon glyphicon-map-marker"></span> ' +
-                event.place + '</div></div><h4 class="event__title">' + event.title + '</h4>'
-            );
-        },
-        dayRender: function(date, cell) {
-            if (cell.hasClass('fc-today')) {
-                var index = cell.index(); // get the td offset
-                // find the corresponding item in header table
-                var header = $('#js-week-calendar thead.fc-head th').eq(index);
-                header.addClass('fc-today'); // update it with a class
-            }
-        }
-    });
+        });
+    }
 
     Tracker.autorun(() => {
         Events.find().fetch();
